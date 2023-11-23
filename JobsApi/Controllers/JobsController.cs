45a1f8a4a9583a4a8f5987e7d61455adb0f92662
Usman Tahir <Usman.tahir@synergy-it.com>
2023-11-23 13:53:27 +0500

    jobsapi setup

---
+++ JobsApi/Controllers/JobsController.cs
@@ -0,0 +1,39 @@
+﻿using JobsApi.Data;
+using JobsApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobsApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobsController : ControllerBase
+    {
+        private readonly JobsContext _jobsContext;
+
+        private readonly ILogger<JobsController> _logger;
+
+        public JobsController(ILogger<JobsController> logger, JobsContext jobsContext)
+        {
+            _logger = logger;
+            _jobsContext = jobsContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Job>> Get()
+        {
+            return await _jobsContext.Jobs.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Job> Get(int id)
+        {
+            return await _jobsContext.Jobs.FirstOrDefaultAsync(x=>x.Id==id);
+        }
+    }
+}
---
+++ JobsApi/Data/JobsContext.cs
@@ -0,0 +1,14 @@
+﻿using JobsApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobsApi.Data
+{
+    public class JobsContext :DbContext
+    {
+        public JobsContext(DbContextOptions<JobsContext> options):base(options)
+        {
+
+        }
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
