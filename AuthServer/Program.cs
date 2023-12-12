a8ae04c94b97d909e8a087f61e4e13067c0126e2
Usman Tahir <Usman.tahir@synergy-it.com>
2023-12-12 09:54:35 +0500

    remove unused namespaces

--- AuthServer/Program.cs
+++ AuthServer/Program.cs
@@ -3,14 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AuthServer
 {
